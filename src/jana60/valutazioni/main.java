fcbdf24855c02ec4bb5ad3ea28261d4965e305f5
paskimeo <paskimeo@gmail.com>
2022-06-16 16:33:39 +0200

    inserito conteggio numeri promossi

--- src/jana60/valutazioni/main.java
+++ src/jana60/valutazioni/main.java
@@ -10,21 +10,29 @@ public class main {
 		Scanner scan = new Scanner(System.in);
 		
 		studenti[] valutazioniStudenti = new studenti[2];
-		
-		for(int i=0; i < valutazioniStudenti.length; i++) {
+		int ragazziPromossi = 0;
+		for(int i=0; i < valutazioniStudenti.length; i++) 
+		{
 			System.out.println("inserisci le assenze dello studente: ");
 			int percentualeAssenze = scan.nextInt();
 			System.out.println("inserisci la media dei voti dello studente: ");
 			int mediaVoti = scan.nextInt();
 			valutazioniStudenti[i]= new studenti(i+1,percentualeAssenze,mediaVoti);
 		
-		
-			if (valutazioniStudenti[i].risulatoFinale()) {
+			
+			
+			if (valutazioniStudenti[i].risulatoFinale()) 
+			{
 				System.out.println("lo studente: " +valutazioniStudenti[i].id + " è stato promosso");
-			}else {
+				 ragazziPromossi++;
+			}else 
+			{
 				System.out.println("lo studente: " +valutazioniStudenti[i].id + " è stato bocciatto");
 			}
+			
+			
 		}
+		System.out.println("i ragazzi promossi sono " + ragazziPromossi);
 	}
 
 }
