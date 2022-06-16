201cfc01ad4e448a83a765f22663c71b4beb2b77
paskimeo <paskimeo@gmail.com>
2022-06-16 14:58:57 +0200

    aggiunto costruttore

--- src/jana60/valutazioni/studenti.java
+++ src/jana60/valutazioni/studenti.java
@@ -6,4 +6,11 @@ public class studenti {
 	int id,percentualeAssenze;
 	double mediaVoti;
 
+	
+	//costruttore
+	studenti(int id, int percentualeAssenze, float mediaVoti) {
+		this.id = id;
+		this.percentualeAssenze = percentualeAssenze;
+		this.mediaVoti = mediaVoti;
+		}
 }
